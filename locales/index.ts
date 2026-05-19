0f0d569529d0e51a14a97c9f71c4b4660f4d0dfc
emmdim <emmdim@users.noreply.github.com>
2026-05-19 16:41:26 +0200

    feat(translations): improves greek translation

--- locales/index.ts
+++ locales/index.ts
@@ -3,14 +3,15 @@ const locales: Locale[] = ['ca', 'de', 'el', 'en', 'es', 'fr', 'it', 'pt' /* 'eu
 const localeDefault: Locale = 'en'
 const availableLocales: { value: Locale; label: string }[] = [
   { value: 'ca', label: 'Català' },
-  { value: 'de', label: 'Deutsch' },
+  // { value: 'de', label: 'Deutsch' },
   { value: 'el', label: 'Ελληνικά' },
   { value: 'en', label: 'English' },
   { value: 'es', label: 'Español' },
   // { value: 'eu', label: 'Euskara' },
-  { value: 'fr', label: 'Français' },
-  { value: 'it', label: 'Italiano' },
-  { value: 'pt', label: 'Português' },
+  // { value: 'fr', label: 'Français' },
+  // { value: 'it', label: 'Italiano' },
+  // { value: 'pt', label: 'Português' },
 ]
 
-export { availableLocales, Locale, localeDefault, locales }
+export { availableLocales, Locale, localeDefault, locales };
+
