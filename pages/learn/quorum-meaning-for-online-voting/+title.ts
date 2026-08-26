0b6864203ad659aae3a8493de22138aadef16aaa
ferranrego <3515290+ferranrego@users.noreply.github.com>
2026-08-26 16:41:51 +0000

    feat(learn): add quorum meaning guide

---
+++ pages/learn/quorum-meaning-for-online-voting/+title.ts
@@ -0,0 +1,5 @@
+import { getMetaByKey } from '@/lib/page-meta'
+
+export default function title(pageContext: Vike.PageContextServer) {
+  return getMetaByKey(pageContext, 'meta.learn.quorum_meaning_for_online_voting.title')
+}
